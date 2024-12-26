ffb28690c1be069911802c0a041688c6a53af88c
Muhammad Abdur Rehman <abdurrehmanrajpoot001@.com>
2024-12-27 03:19:33 +0500

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import Chat from './Chat';
+import Sidebar from './Sidebar';
+import '../styles/Chat.css';
+
+const App = () => {
+  const [users, setUsers] = useState([]);
+  const [selectedUser, setSelectedUser] = useState('');
+  const [receiverId, setReceiverId] = useState('');
+
+  const handleUserSelect = (userId) => {
+    setSelectedUser(userId);
+    setReceiverId(userId);
+    if (!users.includes(userId)) {
+      setUsers((prevUsers) => [...prevUsers, userId]);
+    }
+  };
+
+  const handleDeleteUser = (userId) => {
+    setUsers((prevUsers) => prevUsers.filter(user => user !== userId));
+    // Keep the chat open even if the current user is deleted
+  };
+
+  return (
+    <div id="root">
+      <Sidebar
+        users={users}
+        onUserSelect={handleUserSelect}
+        selectedUser={selectedUser}
+        onDeleteUser={handleDeleteUser}
+      />
+      <div className="chat-container">
+        {receiverId && (
+          <Chat userId={selectedUser} contact={{ _id: receiverId }} />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default App;
